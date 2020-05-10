0977de6992b5f451d0a5d9eaf93b760a4a745002
Chandan Panigrahi <cpanigrahiofficial99@gmail.com>
2020-05-10 23:03:07 +0530

    Added Rounds and game modes

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 class player
 {   private :
-        int player_wins;
+        int player_wins=0;
         char player_symbol;
         string player_name;
 
@@ -31,6 +31,15 @@ class player
             cout<<"wins : "<<player_wins<<endl;
             cout<<"-------------------------------";
         }
+        void winUpdate()
+        {
+            player_wins++;
+        }
+
+        int wins()
+        {
+            return player_wins;
+        }
 
     friend class table;
 
@@ -42,11 +51,19 @@ class table
         char board[3][3];
         int locID;
         int arr[9]={0};
+ 
     public :
+
+        void ResetAll()
+        {
+            for(int i=0; i<9; i++)
+                arr[i]=0;
+        }
+
         void clearQuitCheck()
         {
             char ch;
-            cout<<"Enter Y to Proceed or Q to Quit\n";
+            cout<<"\nEnter Y to Proceed or Q to Quit\n";
             cin>>ch;
             if(ch=='Y' || ch=='y')
                 system("clear");
@@ -238,56 +255,116 @@ int main()
     cout<<"Enter the details of second player >\n";
     p2.getPdata();
 
-    t.clearQuitCheck();
+    int game_type=0, game_rounds=0, invalid_input=0;
+    do{
+        cout<<"====Choose Game format===="<<endl
+        <<"1. One Round battle"<<endl
+        <<"2. Best of three"<<endl
+        <<"3. custom mode"<<endl
+        <<"Enter you option: ";
+        cin>>game_type;
+        switch(game_type)
+        {
+            case 1:
+                game_rounds=1;
+                invalid_input=0;
+                break;
+            case 2:
+                game_rounds=3;
+                invalid_input=0;
+                break;
+            case 3:
+                cout<<"Enter the nuber of Rounds "<<endl;
+                cin>>game_rounds;
+                invalid_input=0;
+                break;
 
-    cout<<"==================**GAME BEGINS**==================\n";
-   
-    t.showloctable();
-    t.reset();
-    cout<<"\n\n\n";
-    t.showtable();
-    int r_check,c_check,d_check;
-    while(over!=0)
+            default:
+                cout<<"Invalid input";
+                invalid_input=1;
+        }
+    }while(invalid_input==1);
+    
+    system("clear");
+
+    for(int i=0; i<game_rounds; i++)
+    {   
+        cout<<endl; 
+        t.clearQuitCheck();
+
+        cout<<"==================**GAME BEGINS**==================\n";
+        cout<<"\nROUND "<<i+1<<" Begins\n\n";
+        t.showloctable();
+        t.reset();
+        cout<<"\n\n\n";
+        t.showtable();
+        int r_check,c_check,d_check;
+        while(over!=0)
+        {
+            cout<<"Player 1|";p1.display_name();cout<<endl;
+            t.matInput(&p1); 
+            t.showloctable();
+            cout<<"\n\n\n";
+            t.showtable();
+            r_check=t.rowcheck();
+            c_check=t.colcheck();
+            d_check=t.diagcheck();
+            if(r_check==1 || c_check==1 || d_check==1)
+            {
+                cout<<"\nGAME OVER\n\n";
+                p1.display_name();
+                cout<<" WON the game\n\n\n\n";
+                p1.winUpdate();
+                over=0;
+                continue;
+            }
+            else
+                t.check_draw();
+
+
+
+            cout<<"Player 2|";p2.display_name();cout<<endl;
+            t.matInput(&p2);
+            t.showloctable();
+            cout<<"\n\n\n";
+            t.showtable();
+            r_check=t.rowcheck();
+            c_check=t.colcheck();
+            d_check=t.diagcheck();
+            if(r_check==1 || c_check==1 || d_check==1)
+            {
+                cout<<"\nGAME OVER\n\n";
+                p2.display_name();
+                cout<<" WON the game\n\n\n\n";
+                p2.winUpdate();
+                over=0;
+            }
+            else
+                t.check_draw();
+
+        }
+        over=1;
+        cout<<"Current game points: "<<endl;
+        p1.showPdata();
+        cout<<endl;
+        p2.showPdata();
+
+        t.ResetAll();                       //reset all the "set" locations of the table for new round
+    }
+    cout<<"\n\n";
+    if(p1.wins() > p2.wins())
     {
-        cout<<"Player 1|";p1.display_name();cout<<endl;
-        t.matInput(&p1); 
-        t.showloctable();
-        cout<<"\n\n\n";
-        t.showtable();
-        r_check=t.rowcheck();
-        c_check=t.colcheck();
-        d_check=t.diagcheck();
-        if(r_check==1 || c_check==1 || d_check==1)
-        {
-            cout<<"\nGAME OVER\n\n";
-            p1.display_name();
-            cout<<" WON the game\n\n\n\n";
-            over=0;
-            continue;
-        }
-        else
-            t.check_draw();
+        p1.display_name();cout<<" is the finall WINNER of this Battle"<<endl;
+    }
 
-
-
-        cout<<"Player 2|";p2.display_name();cout<<endl;
-        t.matInput(&p2);
-        t.showloctable();
-        cout<<"\n\n\n";
-        t.showtable();
-        r_check=t.rowcheck();
-        c_check=t.colcheck();
-        d_check=t.diagcheck();
-        if(r_check==1 || c_check==1 || d_check==1)
-        {
-            cout<<"\nGAME OVER\n\n";
-            p2.display_name();
-            cout<<" WON the game\n\n\n\n";
-            over=0;
-        }
-        else
-            t.check_draw();
-
+    else if(p1.wins() == p2.wins())
+    {
+        cout<<"It's a DRAW"<<endl;
+    }
+    
+    else
+    {
+        p2.display_name();cout<<" is the finall WINNER of this Battle"<<endl;
     }
 
     return 0;
